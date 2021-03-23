dbc935316d0a1117188992b63d743cb9349f92b3
t833w2bma <wsqa_ryo203@yahoo.co.jp>
2021-03-23 19:44:21 +0900

    ao

--- main.php
+++ main.php
@@ -7,8 +7,8 @@
 
 
   final class ChangeModeMain {
-    public static $wp_dir;
-    public static $mdir; 
+    private static $wp_dir;
+    protected static $mdir;
 
 
     function __construct(){
@@ -20,7 +20,7 @@
 
       add_action('admin_print_styles',  function () {
         echo '<style>.pl-1{ padding-left: 1em;} .pl-1 h4{font-size:1.15em;font-weight:600;margin-bottom:.5em;}article.pl-1 {
-          padding-bottom: 2em;}pre.ta-minal {background: #333;width: 563px;padding: 1em;font-size: 1.2em;color: #0f0;font-family: "courier NEW" ,consolas;}
+          padding-bottom: 2em;}pre.ta-minal {background: #333;width: 600px;padding: 1em;font-size: 1.2em;color: #0f0;font-family: "courier NEW" ,consolas;}
         </style>';
       } ); 
        
@@ -29,36 +29,42 @@
 
 
 
-      /*
-        メニューを追加する
-      */
+    /*
+      メニューを追加する
+    */
 
-      public function add_quote_setting(){
-        $my_plugin_slug = self::$mdir ; 
-      
-        // トップレベルにオリジナルのメニューを追加（購読者相当 read editor
-        add_menu_page('CangeMode', 'CangeMode', 'manage_options',
-            $my_plugin_slug,
-            array($this,'ds_form_view'),
-            plugins_url('/images/icon-admin.png', __FILE__),
-            11
-        );
-      }
+    public function add_quote_setting(){
+      $my_plugin_slug = self::$mdir ; 
+    
+      // トップレベルにオリジナルのメニューを追加（購読者相当 read editor
+      add_menu_page('CangeMode', 'CangeMode', 'manage_options',
+          $my_plugin_slug,
+          array($this,'ds_form_view'),
+          plugins_url('/images/icon-admin.png', __FILE__),
+          11
+      );
+    }
 
     public function ds_form_view(){
 
-      if( !empty( $_POST['wps']) ){
+      if( !empty( $_POST['wps']) ){ 
         //wp ディレクトリの権限を変える
-
+        if( $_POST['wps'] == 'Wordpress' ) {
+          $this->wpProtect('570'); //与えない
+          
+        }elseif($_POST['wps'] == 'Other'){
+          $this->wpProtect('770'); //与える
+          echo 'other';
+        }
         
       }elseif(!empty( $_POST['thp'])){
         //テーマ､プラグイン ディレクトリの権限を変える
         if( $_POST['thp'] == 'Wordpress' ) {
-          $this->modUnProtect();
+          $this->modProtect('570'); //与えない
           
         }elseif($_POST['thp'] == 'Other'){
-          $this->modProtect();
-          echo 'othr';
+          $this->modProtect('770'); //与える
+          echo 'other';
         }
 
       }else{
@@ -66,33 +72,28 @@
         echo $_SESSION['cmdx']['results'] ;
       }
       
+      if (isset($_SESSION['cmdx']['results']))
       echo $_SESSION['cmdx']['results'] ;
+
       $_SESSION['cmdx'] = null;
       include 'ds-form-view.php';
-
-    }
-
-    
-
-    /* WPに書き込み権限を与えない*/
-    public function modProtect(){
       
-      shell_exec('chmod -R 570 ' .ABSPATH.'/wp-content');
-      $results = shell_exec('ls -la ' .ABSPATH.'/wp-content');
-      $_SESSION['cmdx']['results'] =  "<pre class='ta-minal'>$results</pre>";
     }
-    
-    
-    
-    /* WPに書き込み権限を与える*/
-    public function modUnProtect(){
-      shell_exec('chmod -R 770 ' .ABSPATH.'/wp-content');
-      $results = shell_exec('ls -la ' .ABSPATH.'/wp-content');
-      $_SESSION['cmdx']['results'] =  "<pre class='ta-minal'>$results</pre>";
-    }
-    
+
     
 
+    /* WPに書き込み権限を与えない570与える770*/
+    protected function modProtect($mod){
+      shell_exec('chmod -R $mod ' .ABSPATH.'/wp-content');
+      $results = shell_exec('ls -la ' .ABSPATH.'/wp-content');
+      $_SESSION['cmdx']['results'] =  "<pre class='ta-minal'>$results</pre>";
+    }
+    protected function wpProtect($wpmod){
+      shell_exec('chmod -R $wpmod ' .ABSPATH);
+      $results = shell_exec('ls -la ' .ABSPATH);
+      $_SESSION['cmdx']['results'] =  "<pre class='ta-minal'>$results</pre>";
+    }
+    
 
   }
   
